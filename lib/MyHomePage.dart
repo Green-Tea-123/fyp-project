57abf07560452a1791188ce9d0a5d711423da6d5
Green-Tea-123 <junboonsoo@gmail.com>
2024-09-24 21:59:31 +0800

    Add dummy nurse login page

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -2,6 +2,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'MyAppState.dart';
+import 'NurseLogin.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -9,41 +10,50 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text('CareSync',
-          style: TextStyle(
-            fontSize: 60,
-          ),
-          textAlign: TextAlign.center,
-          ),
-          Text('Your MIC@Home personal assistant', 
-          textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: EdgeInsets.all(60)
-          ),
-          Padding(
-            padding: EdgeInsets.all(5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('CareSync',
+            style: TextStyle(
+              fontSize: 60,
+            ),
+            textAlign: TextAlign.center,
+            ),
+            Text('Your MIC@Home personal assistant', 
+            textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 100
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(builder: (context) => NurseLogin()));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8, top: 8, left: 50, right: 50),
+                  child: Text('I\'m a Nurse'),
+                ),
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
             child: ElevatedButton(
-              onPressed: () {
-                print('button pressed!');
-              },
-              child: Text('I\'m a Nurse'),
+                onPressed: () {
+                  print('button pressed!');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8, top: 8, left: 50, right: 50),
+                  child: Text('I\'m a Patient'),
+                ),
+              )
             )
-          ),
-          Padding(
-            padding: EdgeInsets.all(5),
-          child: ElevatedButton(
-              onPressed: () {
-                print('button pressed!');
-              },
-              child: Text('I\'m a Patient'),
-            )
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
